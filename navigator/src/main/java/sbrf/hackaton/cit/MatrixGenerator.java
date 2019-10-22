66135603a0bcd8526c922aac0143f18bb79bc0f3
vadelic <komyshenets@mail.ru>
2019-10-22 14:43:40 +0300

    matrix generator

--- navigator/src/main/java/sbrf/hackaton/cit/MatrixGenerator.java
+++ navigator/src/main/java/sbrf/hackaton/cit/MatrixGenerator.java
@@ -1,32 +1,48 @@
 package sbrf.hackaton.cit;
 
 import java.util.Random;
+import java.util.function.Function;
 
 public class MatrixGenerator {
 
-    private Random random = new Random();
     private final int count;
+    private Random random = new Random();
 
     public MatrixGenerator(int count) {
         this.count = count;
     }
 
     public double[] vertexValue(int maxVal) {
-        double[] vertexes = new double[count];
+        return vertex(maxVal, integer -> random.nextDouble() * integer);
+    }
 
+    public double[] vertexValueRound(int maxVal) {
+        return vertex(maxVal, integer -> (double) Math.round(random.nextDouble() * integer));
+    }
+
+    private double[] vertex(int maxVal, Function<Integer, Double> random) {
+        double[] vertexes = new double[count];
         for (int i = 0; i < count; i++) {
-            vertexes[i] = random.nextDouble() * maxVal;
+            vertexes[i] = random.apply(maxVal);
         }
 
         return vertexes;
     }
 
     public double[][] edgesValue(int maxVal) {
+        return edges(maxVal, integer -> random.nextDouble() * integer);
+    }
+
+    public double[][] edgesValueRound(int maxVal) {
+        return edges(maxVal, integer -> (double) Math.round(random.nextDouble() * integer));
+    }
+
+    private double[][] edges(int maxVal, Function<Integer, Double> random) {
         double[][] edges = new double[count][count];
 
         for (int i = 0; i < count; i++) {
             for (int j = i + 1; j < count; j++) {
-                double v = random.nextDouble() * maxVal;
+                double v = random.apply(maxVal);
                 edges[i][j] = v;
                 edges[j][i] = v;
             }
--- navigator/src/test/java/sbrf/hackaton/cit/MatrixGeneratorTest.java
+++ navigator/src/test/java/sbrf/hackaton/cit/MatrixGeneratorTest.java
@@ -2,13 +2,39 @@ package sbrf.hackaton.cit;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class MatrixGeneratorTest {
+
 
     @Test
     public void vertexValue() {
+        MatrixGenerator generator = new MatrixGenerator(20);
+        double[] doubles = generator.vertexValue(50);
+        System.out.println(Arrays.toString(doubles));
+    }
+    @Test
+    public void vertexValueRound() {
+        MatrixGenerator generator = new MatrixGenerator(20);
+        double[] doubles = generator.vertexValueRound(50);
+        System.out.println(Arrays.toString(doubles));
     }
 
     @Test
     public void edgesValue() {
+        MatrixGenerator generator = new MatrixGenerator(20);
+        double[][] doubles = generator.edgesValue(50);
+        System.out.println(Arrays.toString(doubles));
     }
+
+    @Test
+    public void edgesValueRound() {
+        MatrixGenerator generator = new MatrixGenerator(20);
+        double[][] doubles = generator.edgesValueRound(50);
+        for (double[] aDouble : doubles) {
+            System.out.println(Arrays.toString(aDouble));
+
+        }
+    }
+
 }
