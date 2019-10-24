89309f5e8f66b98b2263a1a06deb093bfb7673ed
GreenNun <stas909@gmail.com>
2019-10-24 23:07:48 +0300

    wc configuration

---
+++ navigator/src/main/java/sbrf/hackaton/cit/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package sbrf.hackaton.cit.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import sbrf.hackaton.cit.component.WebSocketHandler;
+
+import javax.validation.constraints.NotNull;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+
+    @Autowired
+    public WebSocketConfig(WebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(@NotNull WebSocketHandlerRegistry registry) {
+        registry
+                .addHandler(webSocketHandler, "/race")
+                .setAllowedOrigins("*");
+    }
+}
