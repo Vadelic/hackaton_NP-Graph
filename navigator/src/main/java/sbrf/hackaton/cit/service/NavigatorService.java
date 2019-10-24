863d58ecbfd26c284a2c6806d675797327e0c9ee
vadelic <komyshenets@mail.ru>
2019-10-25 00:07:49 +0300

    done

--- navigator/src/main/java/sbrf/hackaton/cit/service/NavigatorService.java
+++ navigator/src/main/java/sbrf/hackaton/cit/service/NavigatorService.java
@@ -1,9 +1,18 @@
 package sbrf.hackaton.cit.service;
 
+import sbrf.hackaton.cit.srv.data.MainResponseServer;
+import sbrf.hackaton.cit.srv.data.PointsServer;
+import sbrf.hackaton.cit.srv.data.RoutesServer;
+import sbrf.hackaton.cit.srv.data.TrafficServer;
+
 public interface NavigatorService {
 
-    String createNavigator(String text);
+    void createNavigator(MainResponseServer main);
 
-    String updateTraffic(double[][] traffic);
+    String updateTraffic(TrafficServer traffic);
+
+    void createVertex(PointsServer points);
+
+    void createRoads(RoutesServer routesServer);
 
 }
--- navigator/src/main/java/sbrf/hackaton/cit/service/impl/NavigatorServiceImpl.java
+++ navigator/src/main/java/sbrf/hackaton/cit/service/impl/NavigatorServiceImpl.java
@@ -2,17 +2,42 @@ package sbrf.hackaton.cit.service.impl;
 
 import org.springframework.stereotype.Service;
 import sbrf.hackaton.cit.service.NavigatorService;
+import sbrf.hackaton.cit.srv.data.MainResponseServer;
+import sbrf.hackaton.cit.srv.data.PointsServer;
+import sbrf.hackaton.cit.srv.data.RoutesServer;
+import sbrf.hackaton.cit.srv.data.TrafficServer;
 
 @Service
 public class NavigatorServiceImpl implements NavigatorService {
 
+    MainResponseServer main = null;
+    private PointsServer points;
+    private RoutesServer routesServer;
+
     @Override
-    public String createNavigator(String text) {
-        return null;
+    public void createNavigator(MainResponseServer main) {
+        this.main = main;
     }
 
     @Override
-    public String updateTraffic(double[][] traffic) {
+    public void createVertex(PointsServer points) {
+        this.points = points;
+        buildNavigator();
+
+    }
+
+    @Override
+    public void createRoads(RoutesServer routesServer) {
+        this.routesServer = routesServer;
+        buildNavigator();
+    }
+
+    private void buildNavigator() {
+
+    }
+
+    @Override
+    public String updateTraffic(TrafficServer traffic) {
         return null;
     }
 }
